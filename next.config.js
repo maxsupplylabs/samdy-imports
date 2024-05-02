8383385ab632dea260b8c1ad1f59f03ae53ee7f3
maxsupplylabs <168697595+maxsupplylabs@users.noreply.github.com>
2024-05-02 02:55:44 +0000

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+module.exports = {
+  images: {
+    domains: [
+      "firebasestorage.googleapis.com",
+      "lh3.googleusercontent.com",
+      "images.unsplash.com",
+      "img.freepik.com",
+    ],
+  },
+  reactStrictMode: true,
+};
